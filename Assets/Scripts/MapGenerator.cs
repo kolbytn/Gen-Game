c533a441d39ebc7b56356b6cc1c565c66da2ea86
Kolby Nottingham <kolbytn@gmail.com>
2020-05-01 14:20:12 -0700

    Working map and player

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public PolygonCollider2D confiner;
+    public PlayerController playerController;
+    public int width = 20;
+    public int height = 20;
+    public Vector2 start;
+
+    int tileCount = 2;
+
+    void Start()
+    {
+        GenerateMap();
+        confiner.points = new Vector2[] { new Vector2(0, 0), new Vector2(0, height), new Vector2(width, height), new Vector2(width, 0) };
+        playerController.Teleport(new Vector2(10, 10), Vector2.zero);
+    }
+
+    void GenerateMap()
+    {
+        Tile blank = Resources.Load<Tile>("Tiles/1bit_small_63");
+        Tile ground = Resources.Load<Tile>("Tiles/1bit_small_6");
+        GameObject treePrefab = Resources.Load<GameObject>("Prefabs/tree");
+
+        int[,] map = new int[width, height];
+        for (int i = 0; i < map.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j < map.GetUpperBound(1); j++)
+            {
+                int tileIndex = (int) Math.Floor(UnityEngine.Random.value * tileCount);
+                if (tileIndex == tileCount)
+                {
+                    tileIndex--;
+                }
+
+                if (tileIndex == 0)
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), blank);
+                }
+                else
+                {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), ground);
+                }
+
+                if (UnityEngine.Random.value < .1)
+                {
+                    GameObject treeObject = Instantiate(treePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    Animator animator;
+    Rigidbody2D rigidbody2d;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        // Set look direction
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("MoveX", lookDirection.x);
+        animator.SetFloat("MoveY", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        // Move Player
+        Vector2 position = rigidbody2d.position;
+
+        position = position + move * speed * Time.deltaTime;
+
+        Teleport(position, Vector2.zero);
+
+        // Check for input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Interact");
+
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.1f, lookDirection, 1.5f, LayerMask.GetMask("Breakable"));
+            
+            if (hit.collider != null)
+            {
+                TreeObject treeObject = hit.collider.GetComponent<TreeObject>();
+                if (treeObject != null)
+                {
+                    treeObject.Break();
+                }
+            }
+        }
+    }
+
+    public void Teleport(Vector2 position, Vector2 velocity)
+    {
+        rigidbody2d.MovePosition(position);
+        rigidbody2d.velocity = velocity;
+    }
+}
